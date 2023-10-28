bd29a2e8a1786e82bd743ffef38bd7c9ed293f14
YunYoung Choi <68066598+yunyoung0531@users.noreply.github.com>
2023-10-28 21:49:47 +0900

    [FE] feat: 글 등록 버튼 생성 및 useNavigate로 연결 (#44)
    * [FE] feat: 글 등록 버튼 생성 및 useNavigate로 연결

--- dodamduck_fe/src/SharingBoard.js
+++ dodamduck_fe/src/SharingBoard.js
@@ -1,8 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 function SharingBoard() {
+
+    let navigate = useNavigate();
+    
     return (
         <>
             <div className='library-nav'>
@@ -12,6 +18,10 @@ function SharingBoard() {
             <div className='library-comment2'>
                 교환&나눔
             </div>
+            </div>
+
+            <div className='circle' onClick={()=> {navigate('/sharingPost')}}>
+                <FontAwesomeIcon icon={faPlus} className='plus-sign' />
             </div>
         </>
     )
