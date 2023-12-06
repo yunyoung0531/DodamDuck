7115843a7ed38f0ce3513ef27777ab306e91a607
YunYoung Choi <68066598+yunyoung0531@users.noreply.github.com>
2023-12-06 19:33:36 +0900

    [FE] feat: 장난감 도서관 사진 적용 (#172)
    * [FE] feat: 장난감 도서관 사진 적용

--- dodamduck_fe/src/Library.js
+++ dodamduck_fe/src/Library.js
@@ -11,10 +11,10 @@ function Library() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('https://api.odcloud.kr/api/15044146/v1/uddi:7894ac31-fe17-420a-834a-824c42470e0e_201905301141?page=1&perPage=24&returnType=JSON&serviceKey=TrbCQQGmSeoFYVRPmlICZP8FAGgmE2MzDNTQg%2BsXuVem1UKSEzEnI3My8Ifq6FuxPAfbObQHFQB9hgomJO9NGg%3D%3D', {
+                const response = await axios.get('https://api.odcloud.kr/api/15044146/v1/uddi:7894ac31-fe17-420a-834a-824c42470e0e_201905301141?page=1&perPage=0&returnType=JSON&serviceKey=TrbCQQGmSeoFYVRPmlICZP8FAGgmE2MzDNTQg%2BsXuVem1UKSEzEnI3My8Ifq6FuxPAfbObQHFQB9hgomJO9NGg%3D%3D', {
                     params: {
                         page: 1,
-                        perPage: 24,
+                        perPage: 30,
                         returnType: 'JSON',
                     },
                     headers: {
@@ -22,14 +22,27 @@ function Library() {
                         "accept": "*/*"
                     }
                 });
-                setLibraries(response.data.data);
+                 // 중복된 이름 제거
+                const uniqueNames = Array.from(new Set(response.data.data.map(item => item.장난감명)));
+                console.log('uniqueNames: ', uniqueNames)
+                // 중복된 이름을 제거한 데이터 구성
+                const uniqueData = Array.from(new Set(response.data.data.map(item => item.장난감명)))
+                .map(uniqueName => {
+                    return response.data.data.find(item => item.장난감명 === uniqueName);
+                })  
+                .sort((a, b) => a.장난감명.localeCompare(b.장난감명));
+                console.log('uniqueData: ', uniqueData)
+                // 데이터를 가져온 후 이름순으로 정렬
+                const sortedData = uniqueData.sort((a, b) => a.장난감명.localeCompare(b.장난감명));
+                setLibraries(sortedData);
+                console.log(sortedData)
             } catch (error) {
                 console.error("오류 뜸!! 다시 해보자", error);
             }
         };
         fetchData();
     }, []);
-
+    
     return(
         <>
             <div className='library-nav'>
@@ -48,10 +61,9 @@ function Library() {
                 {/* <Card.Title>{library.관리기관명}</Card.Title> */}
                 <Card.Title style={{ fontSize: '21px'}}>{library.장난감명}</Card.Title>
                 <Card.Body>
-                    <Card.Img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fshopping.phinf.naver.net%2Fmain_3186478%2F31864788721.20220417025129.jpg&type=sc960_832" style={{ width: '180px', height: '180px'}}/>
+                    {/* <Card.Img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fshopping.phinf.naver.net%2Fmain_3186478%2F31864788721.20220417025129.jpg&type=sc960_832" style={{ width: '180px', height: '180px'}}/> */}
+                    <Card.Img src={`http://sy2978.dothome.co.kr/library/${library.장난감명.replace(/\s/g, '')}.jpg`} style={{ width: '180px', height: '180px'}}/>
                     <Card.Text>
-                        {/* Some quick example text to build on the card title and make up the
-                        bulk of the card's content. */}
                         사용연령: {library.사용연령}<br/>
                         대여료: {library.대여료}
                     </Card.Text>
